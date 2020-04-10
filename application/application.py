356f8438fc25dba0f04b9f457d67e50fbb1bd33b
Shreekara  udupa <shreekaraudupa@gmail.com>
2020-04-10 22:40:42 +0530

    changes in application.py

--- application/application.py
+++ application/application.py
@@ -1,1 +1,10 @@
-print('Hello world')
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def mainPage():
+    return render_template("common.html")
+
+if __name__ == "__main__":
+    app.run(port=8000)
