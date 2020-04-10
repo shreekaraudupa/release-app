4906b0c5ca8b5a7571cbd31f8154850852faacf7
Shreekara  udupa <shreekaraudupa@gmail.com>
2020-04-11 01:23:52 +0530

    First day commit

--- application/application.py
+++ application/application.py
@@ -2,9 +2,14 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def mainPage():
-    return render_template("common.html")
+    return render_template("main-page.html")
+
+@app.route("/branch_cut")
+def branch_cut():
+    return render_template("branch-cut.html")
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(debug=True,port=8000)
